5ca316a73d728e26c79ef32c5e406f710c21fc1d
Crystal Li <cl.crystal.li@gmail.com>
2013-11-08 21:31:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeAge::Application.config.secret_token = '820370bd814e9df44c6352fbdc66e913bd6336130547e01c1abf021d2e6f2923757a5b2d3fa70c9e32ea6393ddada208a3329c4096936d44bf31050efb56913b'
